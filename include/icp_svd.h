bd862eba7df38f94d99c3bcff44ae468d1877199
Ayush Garg <ayushgargdroid@gmail.com>
2021-05-26 21:39:54 +0530

    Added normal estimation using Eigen decomposition for Point Clouds

--- include/icp_svd.h
+++ include/icp_svd.h
@@ -9,7 +9,17 @@
 #include <eigen3/Eigen/SVD>
 
 #include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/eigen.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/feature.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/kdtree/flann.h>
+#include <pcl/search/kdtree.h>
 
 #include <math.h>
 
@@ -17,3 +27,4 @@
 
 Eigen::MatrixXf convertPcltoEigen(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud);
 pcl::PointCloud<pcl::PointXYZ>::Ptr convertEigenToPcl(Eigen::MatrixXf inputPoints);
+pcl::PointCloud<pcl::PointNormal>::Ptr computeNormals(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, float searchRadius);
--- src/icp_helper.cpp
+++ src/icp_helper.cpp
@@ -25,3 +25,36 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr convertEigenToPcl(Eigen::MatrixXf inputPoint
 
     return cloud;
 }
+
+pcl::PointCloud<pcl::PointNormal>::Ptr computeNormals(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, float searchRadius) {
+    pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr treeSearch(new pcl::KdTreeFLANN<pcl::PointXYZ>());
+    treeSearch->setInputCloud(inputCloud);
+
+    pcl::PointCloud<pcl::PointNormal>::Ptr normalCloud(new pcl::PointCloud<pcl::PointNormal>());
+    normalCloud->header = inputCloud->header;
+    normalCloud->width = inputCloud->width;
+    normalCloud->height = inputCloud->height;
+    normalCloud->points.reserve(inputCloud->size());
+    
+    for(int i = 0; i < inputCloud->size(); i++) {
+        std::vector<int> indices;
+        std::vector<float> dists;
+        Eigen::Matrix3f covMat;
+        Eigen::Vector4f mean;
+        Eigen::Vector3f::Scalar eigenValues;
+        Eigen::Vector3f eigenVector;
+        pcl::PointXYZ pt = inputCloud->points[i];
+        pcl::PointNormal normalPt;
+        normalPt.x = pt.x;
+        normalPt.y = pt.y;
+        normalPt.z = pt.z;
+        treeSearch->radiusSearch(i, searchRadius, indices, dists);
+        pcl::computeMeanAndCovarianceMatrix(*inputCloud, indices, covMat, mean);
+        pcl::eigen33(covMat, eigenValues, eigenVector);
+        normalPt.normal_x = eigenVector[0];
+        normalPt.normal_y = eigenVector[1];
+        normalPt.normal_z = eigenVector[2];
+        normalCloud->points.emplace_back(normalPt);
+    }
+    return normalCloud;
+}

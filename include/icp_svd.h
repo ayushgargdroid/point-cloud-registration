9568d5d887a0d9750c110f3b5cc1cf590bb3b40a
Ayush Garg <ayushgargdroid@gmail.com>
2021-05-27 08:55:20 +0530

    Cmake precompiled headers magic

--- include/icp_svd.h
+++ include/icp_svd.h
@@ -1,29 +1,12 @@
 #pragma once
 
-#include <iostream>
-#include <iterator>
-#include <random>
-
-#include <eigen3/Eigen/Core>
-#include <eigen3/Eigen/Geometry>
-#include <eigen3/Eigen/SVD>
-
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/common/centroid.h>
-#include <pcl/common/eigen.h>
-#include <pcl/kdtree/kdtree_flann.h>
-
-#include <pcl/features/normal_3d.h>
-#include <pcl/features/feature.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/kdtree/flann.h>
-#include <pcl/search/kdtree.h>
-
-#include <math.h>
+#include <project_includes.h>
 
 #define DEG_TO_RAD(x) (x / 180.0 * M_PI)
+
+struct CorrespondenceData {
+    Eigen::MatrixXf srcPts, dstPts;
+};
 
 Eigen::MatrixXf convertPcltoEigen(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud);
 pcl::PointCloud<pcl::PointXYZ>::Ptr convertEigenToPcl(Eigen::MatrixXf inputPoints);
---
+++ include/project_includes.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <iterator>
+#include <random>
+
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/SVD>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/common/centroid.h>
+#include <pcl/common/eigen.h>
+#include <pcl/common/transforms.h>
+
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/feature.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/kdtree/flann.h>
+#include <pcl/search/kdtree.h>
+
+#include <math.h>

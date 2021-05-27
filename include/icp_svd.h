1eb56f0efa4e06a656cac813bfbd9eb5d6bf3437
Ayush Garg <ayushgargdroid@gmail.com>
2021-05-27 09:15:43 +0530

    Improve precompiled header usage

--- include/icp_svd.h
+++ include/icp_svd.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include <project_includes.h>
-
 #define DEG_TO_RAD(x) (x / 180.0 * M_PI)
-
-struct CorrespondenceData {
-    Eigen::MatrixXf srcPts, dstPts;
-};
 
 Eigen::MatrixXf convertPcltoEigen(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud);
 pcl::PointCloud<pcl::PointXYZ>::Ptr convertEigenToPcl(Eigen::MatrixXf inputPoints);
 pcl::PointCloud<pcl::PointNormal>::Ptr computeNormals(pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, float searchRadius);
+std::tuple<Eigen::MatrixXd, Eigen::MatrixXd> getNearestCorrespondences(pcl::PointCloud<pcl::PointXYZ>::ConstPtr srcCloud, pcl::PointCloud<pcl::PointXYZ>::ConstPtr dstCloud, float maxDist);
